e3400f496dd5911545458c938af77287c33ee5b2
Alex Williams & Mark Pearson <mkb2191@protonmail.com>
2019-08-06 16:42:12 +0100

    Followed static analysis guidelines

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-import pump_control
 import time
 
+import pump_control
+
 def main():
-    pump = pump_control.Pump()  
+    pump = pump_control.Pump()
     for _ in range(8):
         pump.enable_pump_for_duration(2)
         time.sleep(3 * 3600)
-        
+
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -7,28 +7,29 @@ Created on Tue Aug  6 15:38:24 2019
 """
 
 import unittest
-import pump_control
 import time
+
+import pump_control
 
 class TestPumpControl(unittest.TestCase):
     def test_enable_pump_for_duration(self):
         start = time.time()
-        pump = pump_control.Pump()  
+        pump = pump_control.Pump()
         pump.enable_pump_for_duration(2)
         duration = time.time() - start
         self.assertTrue(1.9 < duration < 2.1)
-        
+
     def test_start_pump(self):
-        pump = pump_control.Pump()  
+        pump = pump_control.Pump()
         pump.start_pump()
         self.assertEqual(1, pump.pump.value)
         pump.stop_pump()
 
     def test_stop_pump(self):
-        pump = pump_control.Pump()  
+        pump = pump_control.Pump()
         pump.start_pump()
         pump.stop_pump()
         self.assertEqual(0, pump.pump.value)
-        
+    
 if __name__ == '__main__':
     unittest.main()
