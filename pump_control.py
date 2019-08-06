deb684ee1f95847949b5c17738d242b0f84c6839
Alex Williams & Mark Pearson <mkb2191@protonmail.com>
2019-08-06 16:13:25 +0100

    Added class to pump control

--- pump_control.py
+++ pump_control.py
@@ -7,16 +7,24 @@ Created on Tue Aug  6 15:34:58 2019
 """
 import time
 
-def start_pump():
-    pass
+import gpiozero
 
-def stop_pump():
-    pass
+class Pump:
+    def __init__(self):
+        self.led = gpiozero.LED(17)
+    
 
-def enable_pump_for_duration(duration):
-    start_pump()
-    time.sleep(duration)
-    stop_pump()
-    
-    
-    
+    def start_pump(self):
+        self.led.on()
+
+    def stop_pump(self):
+        self.led.off()
+        
+    def enable_pump_for_duration(self, duration):
+        self.start_pump()
+        time.sleep(duration)
+        self.stop_pump()
+        
+if __name__ == '__main__':
+    pump = Pump()  
+    pump.enable_pump_for_duration(2)
