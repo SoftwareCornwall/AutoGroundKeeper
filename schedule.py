0e66b3a3016cbf62be37ae81b6c8e041ff4ddb42
Alex Williams & Mark Pearson <mkb2191@protonmail.com>
2019-08-07 11:19:06 +0100

    Added schedule API skeleton

---
+++ schedule.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111
+
+class Schedule:
+    def __init__(self):
+        pass
+
+    def set_minimium_watering_frequency(self, gap):
+        pass
+
+    def set_water_dispense_amount(self, amount):
+        pass
+
+    def set_moisture_level_threshold(self, moisture_level):
+        pass
