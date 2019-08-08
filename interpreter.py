9e65b53e11d0eab32c2612b3b74a753155ee1655
= <=>
2019-08-08 10:38:30 +0100

    Added spacing

--- interpreter.py
+++ interpreter.py
@@ -9,17 +9,17 @@ Created on Wed Aug  7 11:14:55 2019
 import spidev
 
 class MoistureInterpreter:
-    
+
     def __init__(self):
         self.moistureSensor = spidev.SpiDev()
         self.moistureSensor.open(0, 0)
         self.moistureSensor.max_speed_hz = 5000
 
     def ReadFromChip(self):
-        dataArray = self.moistureSensor.xfer([0x60,0x00])
+        dataArray = self.moistureSensor.xfer([0x60, 0x00])
         print(self.ConvertData(dataArray))
 
     def ConvertData(self, data_array):
-        return (data_array[0]*256)+(data_array[1])
-        
+        return (data_array[0] * 256) + (data_array[1])
+
         #convert from binary to int
