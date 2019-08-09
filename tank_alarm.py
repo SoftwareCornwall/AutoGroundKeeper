31d71bcc9417e2bd401bcf92b93c1f82ecbc2667
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-09 12:50:51 +0100

    Added tank alarm

---
+++ tank_alarm.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111
+
+class TankAlarm:
+    def __init__(self):
+        pass
+
+    def set_status(self, status):
+        '''Status is current tank water level'''
+        pass
