c8bf6f593388e1f7f854d3b5dfbb51878f034c6a
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-09 13:02:19 +0100

    Added functionality to tank_alarm, added tests

--- tank_alarm.py
+++ tank_alarm.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0111
 
+import gpiozero
+
 class TankAlarm:
     def __init__(self):
-        pass
+        self._green = gpiozero.DigitalOutputDevice(6)
+        self._red = gpiozero.DigitalOutputDevice(26)
+        self.set_status(1)
 
     def set_status(self, status):
         '''Status is current tank water level'''
-        pass
+        if status == 1:
+            self._green.on()
+            self._red.off()
+        else:
+            self._green.off()
+            self._red.on()
