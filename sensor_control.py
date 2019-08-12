4b6e2cb147aa161b4cc2f587caa70f83f1f47aea
Max Oates & Andy Moorhouse <mcoates111@gmail.com & md@coastalit.com>
2019-08-12 13:43:51 +0100

    Chane Refactored sensor control variable names

--- sensor_control.py
+++ sensor_control.py
@@ -12,9 +12,9 @@ import spidev
 
 class Sensor:
     def __init__(self):
-        self.moisture_sensor = spidev.SpiDev()
-        self.moisture_sensor.open(0, 0)
-        self.moisture_sensor.max_speed_hz = 5000
+        self.MCP3002 = spidev.SpiDev()
+        self.MCP3002.open(0, 0)
+        self.MCP3002.max_speed_hz = 5000
 
     def convert_data(self, byte_array):
         '''Strips unneeded bits to avoid errors'''
@@ -23,13 +23,13 @@ class Sensor:
         return byte_1 + byte_2
 
     def get_moisture_a2d(self):
-        data = self.moisture_sensor.xfer([0x60, 0x00])
+        data = self.MCP3002.xfer([0x60, 0x00])
         moisture_level = self.convert_data(data)
         print(moisture_level)
         return moisture_level
 
     def get_light_a2d(self):
-        data = self.moisture_sensor.xfer([0x70, 0x00])
+        data = self.MCP3002.xfer([0x70, 0x00])
         light_level = self.convert_data(data)
         print(light_level)
         return light_level
