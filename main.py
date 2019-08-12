19bf777091f641a7c5ac6b455f10ba682cc3a545
= <=>
2019-08-12 16:50:20 +0100

    Added use of config file

--- main.py
+++ main.py
@@ -14,32 +14,33 @@ import sensor_control
 
 def main():
     schedule = scheduler.Scheduler()
-    sensors = sensor_control.Sensor()
-
-    schedule.register_task('stop', schedule.stop_scheduler)
-    schedule.add_to_schedule('stop', time.time() + 10)  # 86400)
 
     config = config_handler.ConfigHandler()
     schedule.register_task('config_reload', config.run,
                            (schedule, 'config_reload'))
     schedule.add_to_schedule('config_reload', time.time())
 
+    sensors = sensor_control.Sensor()
+
+    schedule.register_task('stop', schedule.stop_scheduler)
+    schedule.add_to_schedule('stop', time.time() + 10)  # 86400)
+
     tank = tank_control.TankControl()
     schedule.register_task(
         'update_leds', tank.run, (schedule, 'update_leds'))
     schedule.add_to_schedule('update_leds', time.time())
 
-    moisture = moisture_check.MoistureCheck(sensors) # needs config object
+    moisture = moisture_check.MoistureCheck(config, sensors)
     schedule.register_task(
         'check_moisture_level',
         moisture.run,
         (schedule,
          'check_moisture_level'))
     schedule.add_to_schedule('check_moisture_level', time.time())
-    
-    recorder = record_data.RecordData(sensors) # needs config object
+
+    recorder = record_data.RecordData(config, sensors)
     schedule.register_task(
-            'update_csv', recorder.run(), (schedule, 'update_csv'))
+        'update_csv', recorder.run(), (schedule, 'update_csv'))
     schedule.add_to_schedule('update_csv', time.time())
     schedule.run_scheduler()
 
--- record_data.py
+++ record_data.py
@@ -2,18 +2,19 @@
 import csv_recording
 import time
 
+
 class RecordData:
     def __init__(self, config, sensor_control):
         self._config = config
         self._csv_file = csv_recording.CSVRecording()
         self._sensor = sensor_control
-        
+
     def add(self):
         moisture_level = self._sensor.get_moisture_a2d()
         light_level = self._sensor.get_light_a2d()
         self._csv_file.add_record(moisture_level, light_level)
-        
+
     def run(self, scheduler, name):
         self.add()
-        scheduler.add_to_schedule(name, time.time() + self._config["record_interval"])
-        
+        scheduler.add_to_schedule(
+            name, time.time() + self._config["record_interval"])
