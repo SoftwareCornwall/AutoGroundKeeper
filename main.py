8fb36a0d858da0672b72d8ce68223dc603a89817
Max Oates & Andy Moorhouse <mcoates111@gmail.com & md@coastalit.com>
2019-08-12 16:46:55 +0100

    Added scheduled csv update

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import time
 
 import tank_control
 import moisture_check
+import record_data
+import sensor_control
 
 
 def main():
     schedule = scheduler.Scheduler()
+    sensors = sensor_control.Sensor()
 
     schedule.register_task('stop', schedule.stop_scheduler)
     schedule.add_to_schedule('stop', time.time() + 10)  # 86400)
@@ -20,14 +23,18 @@ def main():
         'update_leds', tank.run, (schedule, 'update_leds'))
     schedule.add_to_schedule('update_leds', time.time())
 
-    moisture = moisture_check.MoistureCheck()
+    moisture = moisture_check.MoistureCheck(sensors) # needs config object
     schedule.register_task(
         'check_moisture_level',
         moisture.run,
         (schedule,
          'check_moisture_level'))
     schedule.add_to_schedule('check_moisture_level', time.time())
-
+    
+    recorder = record_data.RecordData(sensors) # needs config object
+    schedule.register_task(
+            'update_csv', recorder.run(), (schedule, 'update_csv'))
+    schedule.add_to_schedule('update_csv', time.time())
     schedule.run_scheduler()
 
 
