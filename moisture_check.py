23464dbd354a541520f9c21f70924d436148b4d1
Max Oates & Andy Moorhouse <mcoates111@gmail.com & md@coastalit.com>
2019-08-12 15:56:33 +0100

    Added interval checking to moisture check

--- moisture_check.py
+++ moisture_check.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import time
 import config_handler
 import sensor_control
 
@@ -6,13 +7,17 @@ class MoistureCheck:
     def __init__(self, config):
         self._config = config
         self._moisture_sensor = sensor_control.Sensor()
+        self._last_water = time.time()
         self._moisture_level = 0
         
     def _should_water(self):
-        self._moisture_level = self._moisture_sensor.get_moisture_a2d()        
-        if (self._moisture_level < self._config['moisture_level_threshold'])
-            #call pump schedule
-            pass
+        self._moisture_level = self._moisture_sensor.get_moisture_a2d()
+        moisture_low = (self._moisture_level < self._config['moisture_level_threshold'])
+        interval_exceeded = ((time.time() - self._last_water) > self._config.data['interval'])        
+        if moisture_low and interval_exceeded
+            _last_water = time.time()
+            #call watering function
+            
     
     def run(self, scheduler, name):
         self._should_water()
