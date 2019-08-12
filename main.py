5059ab560291561d5e128d3121786f03b8d8b8a3
= <=>
2019-08-12 16:16:01 +0100

    ./format.sh

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ def main():
     schedule.register_task(
         'update_leds', tank.run, (schedule, 'update_leds'))
     schedule.add_to_schedule('update_leds', time.time())
-    
+
     moisture = moisture_check.MoistureCheck()
     schedule.register_task(
-            'check_moisture_level', moisture.run, (schedule, 'check_moisture_level'))
+        'check_moisture_level',
+        moisture.run,
+        (schedule,
+         'check_moisture_level'))
     schedule.add_to_schedule('update_leds', time.time())
-    
 
     schedule.run_scheduler()
 
--- moisture_check.py
+++ moisture_check.py
@@ -2,26 +2,26 @@
 import time
 import sensor_control
 
+
 class MoistureCheck:
     def __init__(self, config):
         self._config = config
         self._moisture_sensor = sensor_control.Sensor()
         self._last_water = time.time()
         self._moisture_level = 0
-        
+
     def _should_water(self):
         self._moisture_level = self._moisture_sensor.get_moisture_a2d()
-        if (self._moisture_level < self._config['moisture_level_threshold']):      
+        if (self._moisture_level < self._config['moisture_level_threshold']):
             self._last_water = time.time()
-            #call watering function
+            # call watering function
             return 1
         else:
             return 0
-    
+
     def run(self, scheduler, name):
         if self._should_water() == 1:
             wait_time = self._config['interval']
         else:
             wait_time = self._config['check_frequency']
         scheduler.add_to_schedule(name, time.time() + wait_time)
-        
