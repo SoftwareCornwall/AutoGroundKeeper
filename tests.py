8f2f30c2bc3e72d66d5a591dc63fc05da66c78ea
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-13 10:55:27 +0100

    Added water recived aand start/stop test for pump schedule

--- tests.py
+++ tests.py
@@ -100,16 +100,36 @@ class TestPumpControl(unittest.TestCase):
 
 class TestPumpSchedule(unittest.TestCase):
     def test_water_recived_by_sensor(self):
+        
         moisture_sensor = MockSensor()
         start_moisture_level = moisture_sensor.get_a2d_count()
         
         pump = pump_control.Pump()
         
-        pump_sch = pump_schedule.Watering_Schedule(moisture_sensor, pump)
-        pump_sch.enable_pump_until_moisture_sencor_is_saturated_for_duration()
+        with pump_schedule.Watering_Schedule(moisture_sensor, pump) as pump_sch:
+            pump_sch.enable_pump_until_moisture_sencor_is_saturated_for_duration()
+            
         
         self.assertTrue(moisture_sensor.get_a2d_count() > start_moisture_level)
+        
+    def test_pump_starts(self):
+        moisture_sensor = MockSensor()
+        
+        pump = pump_control.Pump()
+        
+        with pump_schedule.Watering_Schedule(moisture_sensor, pump) as pump_sch:
+            self.assertEqual(1, pump.pump.value)
+            pump_sch.enable_pump_until_moisture_sencor_is_saturated_for_duration()
 
+    def test_pump_stops(self):
+        moisture_sensor = MockSensor()
+        
+        pump = pump_control.Pump()
+        
+        with pump_schedule.Watering_Schedule(moisture_sensor, pump) as pump_sch:
+            pump_sch.enable_pump_until_moisture_sencor_is_saturated_for_duration()
+            
+        self.assertEqual(0, pump.pump.value)
 
 class TestTankAlarm(unittest.TestCase):
     def setUp(self):
