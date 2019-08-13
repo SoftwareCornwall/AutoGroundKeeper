04ad5f95e0f28c6a2e2599c564e058076031b4fb
= <=>
2019-08-13 11:49:28 +0100

    Changed function names to be more clear

--- record_data.py
+++ record_data.py
@@ -9,12 +9,12 @@ class RecordData:
         self._csv_file = csv_recording.CSVRecording()
         self._sensor = sensor_control
 
-    def add(self):
+    def append_current_readings_to_csv(self):
         moisture_level = self._sensor.get_moisture_a2d()
         light_level = self._sensor.get_light_a2d()
         self._csv_file.add_record(moisture_level, light_level)
 
     def run(self, scheduler, name):
-        self.add()
+        self.append_current_readings_to_csv()
         scheduler.add_to_schedule(
             name, time.time() + self._config["record_interval"])
