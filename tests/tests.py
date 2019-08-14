c7945d4f72d4b30cebae891d605b26e165d75e56
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-14 12:18:47 +0100

    Moved tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,8 @@ import sys
 sys.path.append('.')
 
 import scheduler_test
+import tests_old
 
 
 if __name__ == '__main__':
-    unittest.main(defaultTest=['scheduler_test'])
+    unittest.main(defaultTest=['scheduler_test', 'tests_old'])
