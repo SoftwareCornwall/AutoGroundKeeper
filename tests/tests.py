facd5f6d0397b249b424f764b95a6ac054b46ffd
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-14 13:05:55 +0100

    Renamed test

--- tests/tests.py
+++ tests/tests.py
@@ -4,13 +4,14 @@ import sys
 
 sys.path.append('.')
 
-import tests_old
+import pump_control_tests
 import pump_schedule_tests
 import sensor_tests
 import scheduler_test
 import csv_tests
 
 if __name__ == '__main__':
-    unittest.main(defaultTest=['scheduler_test', 'sensor_tests', 'tests_old',
+    unittest.main(defaultTest=['scheduler_test', 'sensor_tests',
+                               'pump_control_tests',
                                'csv_tests', 'pump_schedule_tests'])
 
