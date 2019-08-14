9ef1cab94ab54d80ee9ea5c905e8c228166a6911
= <=>
2019-08-14 15:12:54 +0100

    Added POC email sending, tested and achieved success

--- email_handler.py
+++ email_handler.py
@@ -1,20 +1,69 @@
 # -*- coding: utf-8 -*-
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email.utils
+import smtplib
+
+from os.path import basename
+import subprocess
+import re
+
 
 
 class EmailHandler:
     def __init__(self, config):
         self._config = config
-
-    def send(
-            self,
-            to_address,
-            from_address,
-            from_name,
-            subject,
-            body,
-            attachments):
-        print('To:', to_address)
-        print('From: %s <%s>' % (from_name, from_address))
-        print('Subject:', subject)
-        print('Body:', body)
-        print('Attachments:', attachments)
+    
+    
+    @staticmethod
+    def mxlookup(domain):
+        try:
+            data = subprocess.getoutput('host '+domain)
+            data = re.findall('mail is handled by (.*?) (.*?)\n', data+'\n')
+            return min([int(i[0]), i[1]] for i in data)[1]
+        except ValueError:
+            pass
+        return domain
+    
+    @staticmethod
+    def send_email(rcpttos, fromaddr, fromname, subject, body, attachments):
+        mimeapps = []
+        for f in attachments:
+            try:
+                with open(f, 'rb') as cfile:
+                    app = MIMEApplication(cfile.read())
+                    app['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+                    mimeapps.append(app)
+            except Exception as error:
+                print(error)
+        print('Fetched attachments')
+        addrlist = {}
+        for toaddr in rcpttos:
+            try:
+                server = EmailHandler.mxlookup(toaddr.split('@')[1])
+                addrlist[server] = addrlist.get(server, [])+[toaddr]
+            except IndexError:
+                print('Invalid email:', toaddr)
+        print('Looked up addresses')
+        for server in addrlist:
+            toaddrs = addrlist[server]
+            msg = MIMEMultipart()
+            msg['To'] = ', '.join(toaddrs)
+            msg['From'] = email.utils.formataddr((fromname, fromaddr))
+            msg['Subject'] = subject
+            msg.attach(MIMEText(body))
+            for app in mimeapps:
+                msg.attach(app)
+            try:
+                smtp = smtplib.SMTP(server)
+                print('Connected to', server)
+                try:
+                    smtp.starttls()
+                    print('Started TLS')
+                except Exception as error:
+                    print(error)
+                smtp.send_message(msg)
+                print('Sent')
+            except Exception as error:
+                print(error)
