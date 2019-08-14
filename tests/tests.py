3623ab92ffe3f8de29a0f1980e61eccf9a5a792c
= <=>
2019-08-14 16:17:59 +0100

    Added test

--- tests/tests.py
+++ tests/tests.py
@@ -9,9 +9,11 @@ import pump_schedule_tests
 import sensor_tests
 import scheduler_test
 import csv_tests
+import tank_control_test
 
 if __name__ == '__main__':
     unittest.main(defaultTest=['scheduler_test', 'sensor_tests',
                                'pump_control_tests',
-                               'csv_tests', 'pump_schedule_tests'])
+                               'csv_tests', 'pump_schedule_tests',
+                               'tank_control_test'])
 
