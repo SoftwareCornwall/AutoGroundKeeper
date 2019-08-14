01a7a1d8602bc1cf6d845b28f9a196a6fe702e9d
= <=>
2019-08-14 13:32:15 +0100

    Added user json file

--- config_handler.py
+++ config_handler.py
@@ -7,20 +7,20 @@ import os
 
 
 class ConfigHandler:
-    def __init__(self, config_file='succulent.json'):
-        self.file_location = config_file
+    def __init__(self):
+        self.file_location = self.get_user_preferences()
         self.load(self.file_location)
         self.disable_reload = False
 
     def load(self, config_file):
-        with open(self.file_location) as file:
+        with open(config_file) as file:
             self.data = json.load(file)
         self.last_loaded = time.time()
 
     def reload_if_modified(self):
         if not self.disable_reload and os.path.getmtime(
                 self.file_location) > self.last_loaded:
-            self.load()
+            self.load(self.file_location)
 
     def __getitem__(self, key):
         try:
@@ -31,3 +31,8 @@ class ConfigHandler:
     def run(self, scheduler, name):
         self.reload_if_modified()
         scheduler.add_to_schedule(name, time.time() + 5)
+
+    def get_user_preferences(self):
+        with open('user_preferences.json') as file:
+            config = json.load(file)
+        return config['Plant_Type']
