c3da29d38d4b8cf30cbe2fae3351fdad5bae8cf8
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-14 12:35:43 +0100

    Moved csv tests

--- tests/tests.py
+++ tests/tests.py
@@ -4,10 +4,14 @@ import sys
 
 sys.path.append('.')
 
-import scheduler_test
-import sensor_tests
 import tests_old
+import sensor_tests
+import scheduler_test
+import csv_tests
+
+
 
 
 if __name__ == '__main__':
-    unittest.main(defaultTest=['scheduler_test', 'sensor_tests', 'tests_old'])
+    unittest.main(defaultTest=['scheduler_test', 'sensor_tests', 'tests_old',
+                               'csv_tests'])
