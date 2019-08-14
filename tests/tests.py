fc7f46e66398d3a9403e3dcba9e106625163705d
Rhian Rushbrook & Ashley Sands <py@bluefrite.com>
2019-08-14 12:31:38 +0100

    Finished moving sensor tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,8 +5,9 @@ import sys
 sys.path.append('.')
 
 import scheduler_test
+import sensor_tests
 import tests_old
 
 
 if __name__ == '__main__':
-    unittest.main(defaultTest=['scheduler_test', 'tests_old'])
+    unittest.main(defaultTest=['scheduler_test', 'sensor_tests', 'tests_old'])
--- tests/tests_old.py
+++ tests/tests_old.py
@@ -13,7 +13,6 @@ import time
 
 import pump_control
 import pump_schedule
-import sensor_control
 import tank_alarm
 import csv_recording
 
@@ -183,30 +182,6 @@ class TestTankAlarm(unittest.TestCase):
         self.tank_alarm.set_status(0)
         self.assertEqual(0, self.tank_alarm._green.value)
         self.assertEqual(1, self.tank_alarm._red.value)
-
-
-class MockSPI():
-    def xfer(self, transmitted_data):
-        self.transmitted = transmitted_data
-        return [0, 0]
-
-
-class TestMoistureSensorInOut(unittest.TestCase):
-    def setUp(self):
-        self.interp = sensor_control.Sensor()
-        self.interp.MCP3002 = MockSPI()
-
-    def test_data_is_converted_correctly(self):
-        mock_data = [0b00000010, 0b11101011]
-        self.assertEqual(0b1011101011, self.interp.convert_data(mock_data))
-
-    def test_moisture_reading_is_taken_from_channel_0(self):
-        self.interp.get_moisture_a2d()
-        self.assertEqual([0x60, 0x00], self.interp.MCP3002.transmitted)
-
-    def test_light_reading_is_taken_from_channel_1(self):
-        self.interp.get_light_a2d()
-        self.assertEqual([0x70, 0x00], self.interp.MCP3002.transmitted)
 
 
 class TestCSV(unittest.TestCase):
