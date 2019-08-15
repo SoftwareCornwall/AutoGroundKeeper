994b2041c61c1ea29a96c45a0602cbf8f43381d2
= <=>
2019-08-15 10:08:44 +0100

    changed setup to check for updates

--- setup_script.sh
+++ setup_script.sh
@@ -1,4 +1,6 @@
-sudo apt install spyder3 gitk flake8 python3-matplotlib libreoffice
+sudo apt update
+sudo apt upgrade -y
+sudo apt install spyder3 gitk flake8 python3-matplotlib libreoffice -y
 pip3 install spyder-unittest
 pip3 install --upgrade autopep8
 pip3 install --upgrade coverage
